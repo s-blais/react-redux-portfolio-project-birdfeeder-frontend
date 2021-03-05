1fb11c00e14281f3e534932906b4c4d6ba7fba56
s-blais <stephen@s-blais.com>
2021-03-05 14:55:07 -0500

    Implement fetchBirds on App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './components/Home'
 import BirdsIndex from './components/BirdsIndex'
+import { connect } from 'react-redux'
+import { fetchBirds } from './redux/actions/birdActions'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Nav />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/birds' component={BirdsIndex} />
-        </Switch>
-
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  
+  componentDidMount() { this.props.fetchBirds() }
+  
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/birds' component={BirdsIndex} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect(null, { fetchBirds })(App)
