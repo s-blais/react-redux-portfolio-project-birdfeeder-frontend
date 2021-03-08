a57a56b63b3b357edf504c3272935c797cd9554c
s-blais <stephen@s-blais.com>
2021-03-08 15:58:09 -0500

    connect to Redux store to access bird img url

--- src/components/DayDisplay.js
+++ src/components/DayDisplay.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function DayDisplay(props) {
+class DayDisplay extends React.Component {
 
-  return (
-    <div>
-      <b>{props.day.attributes.date}</b>
-    </div>
-  )
+  birdThumbnails = (ids) => {
+    return (
+      ids.map(id => {
+        const bird = this.props.birds.find(b => b.id == id)
+        const url = bird.attributes.image_url.replace("nas_bird_teaser_illustration", "bird_illustration")
+        return <img src={url} style={{maxHeight: "60px", maxWidth: "60px", margin: "5px 5px 10px 10px"}} />
+      })
+    )
+  }
 
+  render() {
+    return (
+      <div>
+        <b>{ this.props.day.attributes.date }</b>
+        <br />
+        { this.birdThumbnails(this.props.day.attributes.birds_ids) }
+      </div>
+    ) 
+  }
 }
+
+const mapStateToProps = state => { 
+  return { birds: state.birds }
+}
+
+export default connect(mapStateToProps)(DayDisplay)
