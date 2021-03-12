829f0e6249d915251ad9b6163cde6254ad0f3912
s-blais <stephen@s-blais.com>
2021-03-12 14:25:07 -0500

    Initial styling of bird cards

--- src/components/BirdCard.js
+++ src/components/BirdCard.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+const cardStyle = {
+  width: "140px",
+  borderRadius: "10px",
+  margin: "5px",
+  padding: "10px",
+  textAlign: "center",
+  backgroundColor: "#e0e0e0",
+  display: "inline-block"
+}
+
+const imageStyle= {
+  maxWidth: "130px",
+  maxHeight: "150px",
+  borderRadius: "5px"
+}
+
 export default function BirdCard(props) {
 
   return (
-    <div>
+    <div style={cardStyle}>
+      <img src={props.bird.attributes.image_url} 
+        style={imageStyle} 
+        alt={props.bird.attributes.name} />
+      <br />
       <b>{props.bird.attributes.name}</b>
     </div>
   )
