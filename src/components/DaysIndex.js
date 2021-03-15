24483a9707adfe1c600a22a92f7b9d0ce38fbffb
s-blais <stephen@s-blais.com>
2021-03-15 13:14:14 -0400

    Move /bird nested route to BirdIndex

--- src/components/DaysIndex.js
+++ src/components/DaysIndex.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import DayDisplay from './DayDisplay'
+import DayForm from './DayForm'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, Route, Switch } from 'react-router-dom'
 
 class DaysIndex extends React.Component {
   render() {  
     return (
-      <div>
-        <h1>Sightings by Day</h1>
-        <Link to='/days/new'>Record a day's sightings</Link>
-        { this.props.days.map(day => <DayDisplay day={day} key={day.id} />)}
-      </div>
+      <Switch>
+        <Route path={`${this.props.match.url}/new`} component={DayForm} />
+        <Route exact path={this.props.match.url} render={() => (
+          <div>
+            <h1>Sightings by Day</h1>
+            <Link to='/days/new'>Record a day's sightings</Link>
+            { this.props.days.map(day => <DayDisplay day={day} key={day.id} />)}
+          </div>
+        )} />
+      </Switch> 
     )
   }
 }
