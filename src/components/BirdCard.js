9b1d83c60d0a9b760668c47c1c3cae2b83f457cc
s-blais <stephen@s-blais.com>
2021-03-19 11:02:15 -0400

    move bird-card css to stylesheet

--- src/components/BirdCard.js
+++ src/components/BirdCard.js
@@ -1,31 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const cardStyle = {
-  width: "140px",
-  borderRadius: "10px",
-  margin: "5px",
-  padding: "10px",
-  textAlign: "center",
-  backgroundColor: "#e0e0e0",
-  display: "inline-block"
-}
-
-const imageStyle= {
-  maxWidth: "130px",
-  maxHeight: "150px",
-  borderRadius: "5px"
-}
-
 const BirdCard = (props) => {
 
   const url = props.bird.attributes.image_url.replace("bird_illustration", "nas_bird_teaser_illustration")
 
   return (
     <Link to={`/birds/${props.bird.id}`}>
-      <div style={cardStyle}>
+      <div className="bird-card">
         <img src={url} 
-          style={imageStyle} 
           alt={props.bird.attributes.name} />
         <br />
         <b>{props.bird.attributes.name}</b>
